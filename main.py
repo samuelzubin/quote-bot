c158701a12eb4dacb3cb14b9e08efa9dfcd5d11b
samuelzubin <szubin06@gmail.com>
2025-10-01 17:12:06 -0700

    Extended web scraper to all pages of website

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 
+#Website URL
+main_url: str = "https://quotes.toscrape.com/"
+
+data = {}
+
 def main() -> None:
-    URL = "https://quotes.toscrape.com/"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-    quote_container: list = soup.find_all("div", class_="col-md-8")
-    quote_container.pop(0)
-    for div in quote_container:                               #Iterate through the one item in quote_container
-        quotes: list = div.find_all("span", class_="text")    #Get all span elements on the page
-        for quote in quotes:                                  #Iterate through all span elements
-            print(quote.text.strip())
-            print("\n")
+    url: str = main_url
+    page_number: int = 1
+
+    while True:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        #Get quotes and authors
+        quote_divs: list = soup.find_all("div", class_="quote")
+        for div in quote_divs:
+            quote: str = div.find("span", class_="text").text.strip()
+            author: str = div.find("small", class_="author").text.strip()
+            
+            #Append to dictionary
+            data.update({quote: author})
+
+            # print(f"{quote} by {author}")
+
+        #Check if there are more pages, break if not
+        next_page = True if (soup.find("li", class_="next") != None) else False
+        if not next_page:
+            break
+
+        #Go to next page if there is one
+        page_number += 1
+        url = main_url + "page/" + str(page_number)
 
 if __name__ == "__main__":
     main()

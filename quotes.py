32994995e2fb320de5a401bb5c095af61e82070f
samuelzubin <szubin06@gmail.com>
2025-10-02 10:38:57 -0700

    Changed choice of quote site. Updated quote scope to multiple authors

--- quotes.py
+++ quotes.py
@@ -2,44 +2,47 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
-#Website URL
-main_url: str = "https://quotes.toscrape.com/"
-
+#Website URLs
+author_pages = ['aristotle', 'marcus-aurelius', 'charles-darwin', 'hank-aaron', 'rené-descartes']
+main_url: str = "https://libquotes.com/"
 
 def fetch_quote() -> list:
     data = []
-    url: str = main_url
-    page_number: int = 1
+    for current_author in author_pages:
+        page_number: int = 1
 
-    while True:
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, "html.parser")
+        while True:
+            url: str = main_url + current_author
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, "html.parser")
 
-        #Get quotes and authors
-        quote_divs: list = soup.find_all("div", class_="quote")
-        for div in quote_divs:
-            quote: str = div.find("span", class_="text").text.strip()
-            author: str = div.find("small", class_="author").text.strip()
-            
-            #Append to list
-            data.append([quote, author])
+            #Get quotes and authors
+            quote_divs: list = soup.find_all("div", class_="panel-body")
+            for div in quote_divs:
+                quote: str = div.find("span", class_="quote_span").text.strip()
+                author: str = div.find("span", class_="fda").text.strip()
+                
+                #Append to list
+                data.append([quote, author])
 
-            # print(f"{quote} by {author}")
+            #Check if there are more pages, break if not
+            if current_author == 'aristotle':
+                next_page = True if page_number < 20 else False
+            elif current_author == 'marcus-aurelius':
+                next_page = True if page_number < 18 else False
+            else:
+                next_page = True if page_number < 13 else False
+                
 
-        #Check if there are more pages, break if not
-        next_page = True if (soup.find("li", class_="next") != None) else False
-        
-        #Go to next page if there is one
-        page_number += 1
-        url = main_url + "page/" + str(page_number)
+            #Go to next page if there is one
+            page_number += 1
+            url = main_url + str(page_number)
 
-        if not next_page:
-            break
+            if not next_page:
+                break
 
     return data if data else []
     
-
-def get_random_quote(quote_list, user_input) -> str:    
+def get_random_quote(quote_list) -> str:    
     random_value = random.randint(0, len(quote_list))
-    
-    return (f"*{quote_list[random_value][0]}*\n-Jaquavius")
+    return (f"*{quote_list[random_value][0]}*\n-{quote_list[random_value][1]}")
--- responses.py
+++ responses.py
@@ -4,7 +4,7 @@ quotes = []
 
 def get_response(user_input: str) -> str:
     global quotes
-    if 'jaq' in user_input.lower() or 'quote':
+    if 'jaq' in user_input.lower() or 'quote' in user_input.lower():
         if not quotes:
             quotes = fetch_quote()
-        return get_random_quote(quotes, user_input)
+        return get_random_quote(quotes)
